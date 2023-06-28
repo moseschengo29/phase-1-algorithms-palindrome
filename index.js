00e4d127ed2df7b20e7951d45906f98a2e730550
Moses <mochengo29@gmail.com>
2023-06-28 19:52:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanStr = word.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  // Compare the reversed string with the original string
+  const reversedStr = cleanStr.split("").reverse().join("");
+  return cleanStr === reversedStr;
 }
 
 /* 
   Add your pseudocode here
+  the function accepts a word when it is called as it
+  s argument and uses the word to make the checks
+
+  it checks if it's the same both when going forward and back wards and returns true or false
+  
 */
 
 /*
   Add written explanation of your solution here
+  the function takes in the word we want to check and then will do something with it
+
+  it then removes non-alphanumeric characters and convert to lowercase
+
+  then it splits it and reverses it and joins, then it compares it to the result to the word then returns true or false
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,6 +30,12 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("test"));
 
   console.log("");
 
